c313053819d9463b7d71c97861bc441cd61095b4
Ahmed BOUSSI <97151432+AhmedBoussi@users.noreply.github.com>
2022-05-05 10:28:23 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+def test_add_integers(self):  
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.add2(1, 2)
+        self.assertEqual(result, 3)
+    
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.add2(1, 2)
+        self.assertEqual(result, 3)
